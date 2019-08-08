66f65b5835c1f2c329caa29d40213b6d784b94ed
Jonathan Azali <azali.jonathan@gmail.com>
2019-08-08 13:07:41 -0700

    tic tac toe jest testing completed

--- __tests__/win.test.js
+++ __tests__/win.test.js
@@ -41,3 +41,51 @@ describe('isDiagonalWin', () => {
     expect(result).toBeFalsy();
   });
 });
+
+describe('isWin', () => {
+  it('should return a false if the input board does not contain a win', () => {
+    const boardArray = [['X', 'O', 'X'], ['X', 'O', 'O'], ['O', 'X', 'X']];
+    const result = win.isWin(boardArray);
+    expect(result).toBeFalsy();
+  });
+
+  it('should return true if the input board contains diagonal win', () => {
+    const boardArray = [['X', 'O', 'X'], ['X', 'X', 'O'], ['O', 'X', 'X']];
+    const result = win.isWin(boardArray);
+    expect(result).toBeTruthy();
+  });
+
+  it('should return true if the input board contains horizontal win', () => {
+    const boardArray = [['X', 'X', 'X'], ['X', 'O', 'O'], ['O', 'X', 'O']];
+    const result = win.isWin(boardArray);
+    expect(result).toBeTruthy();
+  });
+
+  it('should return a true if the input board contains vertical win', () => {
+    const boardArray = [['X', 'O', 'X'], ['X', 'O', 'O'], ['X', 'X', 'O']];
+    const result = win.isWin(boardArray);
+    expect(result).toBeTruthy();
+  });
+});
+
+describe('isTie', () => {
+  it('should return true if there is no wins in a completed input board', () => {
+    const boardArray = [['X', 'O', 'X'], ['X', 'O', 'O'], ['O', 'X', 'O']];
+    const result = win.isTie(boardArray);
+    expect(result).toBeTruthy();
+  });
+
+  it('should return false if there is a space in an input board', () => {
+    const boardArray = [['X', 'O', 'X'], [' ', 'O', 'O'], ['X', 'X', 'O']];
+    const result = win.isTie(boardArray);
+    expect(result).toBeFalsy();
+  });
+
+  /* test case doesn't work because isTie returns true if board is completed
+  NOT if its a tie between players */
+  it('should return false if there is a win in a completed input board', () => {
+    const boardArray = [['X', 'O', 'X'], ['X', 'O', 'O'], ['X', 'X', 'O']];
+    const result = win.isTie(boardArray);
+    expect(result).toBeTruthy();
+  });
+});
